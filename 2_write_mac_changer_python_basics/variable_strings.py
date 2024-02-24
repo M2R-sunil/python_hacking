ae036e80c57080f9dc88986c6dea57c35bf679f4
Sunil9001934 <108231917+Sunil9001934@users.noreply.github.com>
2024-02-24 09:34:49 +0530

    Delete 2_write_mac_changer_python_basics directory

--- 2_write_mac_changer_python_basics/variable_strings.py
+++
@@ -1,8 +0,0 @@
-#!usr/bin/env python2
-import subprocess
-
-interface = "wlan0"
-new_mac = "00:88:77:99:55:55"# thsi is a variable and value 
-
-print("[+] Changing Mac address for " + interface + " to new_mac")
-
