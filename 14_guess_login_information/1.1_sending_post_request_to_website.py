0ae4f17c369357cfbb4aed7e2101b5187cef981d
Sunil9001934 <108231917+Sunil9001934@users.noreply.github.com>
2024-10-19 18:58:17 +0530

    Add files via upload
    gussing login information

---
+++ 14_guess_login_information/1.1_sending_post_request_to_website.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import requests
+
+target_url = "http://192.168.112.128/dvwa/login.php"
+data_dict = {"username": "admin", "password": "password", "Login": "submit"}
+response = requests.post(target_url, data=data_dict)
+print(response.content)
+
+
+
